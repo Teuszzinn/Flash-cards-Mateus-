e5c01bdb33a9fe190cddc641ca19f12b3426941d
Teuszzinn <carvalho.leao@escola.pr.gov.br>
2025-09-09 11:47:09 +0000

    0000

---
+++ Perguntas.js
@@ -0,0 +1,180 @@
+criaCartao(
+'conhecimentos gerais',
+'Qual é o maior país do mundo em extensão territorial? ',
+'Russia'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Quem foi o autor da teoria da gravidade? ',
+'Isaac Newton'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Em que continente fica a Cordilheira dos Andes',
+'América do Sul'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Qual é a capital da Austrália?',
+'Camberra'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Qual elemento químico tem o símbolo "O"?',
+'Oxigênio'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Quem pintou a obra "A Última Ceia"?',
+'Leonardo da Vinci'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Qual foi o primeiro país a conquistar a independência na América Latina? ',
+'Haiti (em 1804)'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Em que ano o homem pisou na Lua pela primeira vez?',
+'1969'
+)
+
+
+criaCartao(
+'conhecimentos gerais',
+'Qual é o maior órgão do corpo humano?',
+' A pele'
+)
+
+criaCartao(
+'conhecimentos gerais',
+'Qual é o idioma mais falado no mundo, considerando número total de falantes? ',
+'Inglês'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
